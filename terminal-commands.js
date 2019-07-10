8c7f31d6e270e9c4fef5592e8c0c789db6b80d25
郷大助 <daisukegoh@guest1-74.csl.sony.co.jp>
2019-07-10 17:22:30 +0900

    20190710

--- terminal-commands.js
+++ terminal-commands.js
@@ -1,11 +1,30 @@
-const fs = require('fs');
+const fs = require('fs')
 
 module.exports.ls = () => {
-    fs.readdir('./', (err, files) => {
-        const filesToString = files.reduce((acc, file) => {
-            return `${acc} ${file} `;
-          }, '');
-       
-          console.log(filesToString);
-    });
+  fs.readdir('./', (err, files) => {
+    const filesToString = files.reduce((acc, file) => {
+      return `${acc} ${file} `;
+    }, '');
+
+    console.log(filesToString);
+  });
+};
+
+module.exports.touch = () => {
+  // your code here
+const filename = 'file.txt';
+fs.closeSync(fs.openSync(filename, 'w'));
+};
+
+module.exports.mkdir = () => {
+  // your code here
+  fs.mkdir(process.argv[2], function (err) {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    else {
+        console.log('finished!!');
+    }
+})
 };
